a4bdd4b7b23087d501a64d67c8f93b7d571451f2
Vineet Dwivedi <vineetdwivedi409@gmail.com>
2026-07-07 20:46:58 +0530

    Config fixes

--- Docker/FRONTEND/vite.config.js
+++ Docker/FRONTEND/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api':{
+        target: 'http://backend:3000',
+        changeorigin: true,
+        secure: false,
+      }
+    }
+  }
 })
