211c1eb73a848c5ceb89132df921304e16b03c3c
Vineet Dwivedi <vineetdwivedi409@gmail.com>
2026-07-07 20:37:09 +0530

    Frontend with docker addon

--- Docker/BACKEND/src/app.js
+++ Docker/BACKEND/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.status(200).json("Welcome To Docker");
@@ -10,12 +12,22 @@ app.get("/api/health",(req,res)=>{
     res.status(200).json("Server is working totally fine!!");
 })
 
-app.get("/api/data",(req,res)=>{
+app.get("/api/users",(req,res)=>{
     const data = [{
         id : 1,
         name : 'Vyuk',
         age : '22',
-}]
+},{
+        id : 2,
+        name : 'John',
+        age : '25',
+},{
+        id : 3,
+        name : 'Doe',
+        age : '30',
+}
+
+]
     res.status(200).json(data);
 })
 
